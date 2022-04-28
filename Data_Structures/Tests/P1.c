38b57a9aff6cce5656263509a2d385d3656c7766
travizanutto <ctravizanutto@gmail.com>
2022-04-28 13:20:57 -0300

    Folder

---
+++ Data_Structures/Tests/P1.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct produto{
+char nomeproduto[20];
+float valor;
+}TProduto;
+
+typedef struct setor {
+     char nomesetor[10];
+     int qtde; 
+     TProduto *prod;
+}TSetor;
+
+
+TSetor *aloca_setor(int tam);
+void armazena_setor_produto(TSetor *paux, int index);
+TSetor *desaloca_setor(TSetor *p, int tam);
+
+TProduto *armazena_produto(int qtd, int index);
+void handle_error();
+
+int main()
+{
+    int tam = 0, op;
+	TSetor *p = NULL;
+    
+	do {
+	    printf("\n------------------------------------");
+	    printf("\n1-Aloca Setor\n2-Armazena Produto\n3-Desaloca Setor\n0-Sair\nInforme Opcao: ");
+    	scanf("%d", &op);
+	    switch(op) {
+	    	case 1: {
+			    printf("Informe numero de setores do supermercado: ");
+	    		scanf("%d",&tam);
+	    		p=aloca_setor(tam);
+	    	} continue;
+	    	case 2: {
+	    		for (int i = 0; i < tam; i++) {
+	    			armazena_setor_produto(p + i, i + 1);
+	    		}
+	    	} continue;
+	    	case 3: {
+	    		p=desaloca_setor(p,tam);
+	    	} continue;
+			case 0: break;
+	    	default: {
+				printf("\nOpcao invalida");
+			} continue;
+		}
+	} while (op != 0);
+    return 0;
+}
+
+TSetor *aloca_setor(int tam)
+{
+    TSetor *paux;
+    paux = (TSetor *) malloc(tam*sizeof(TSetor));
+    if(paux == NULL) handle_error();    
+    return paux;
+}
+
+void armazena_setor_produto(TSetor *paux, int index) {
+    int qtd;
+
+	printf("Informe o nome do setor %i: ", index);
+	scanf("%s", paux->nomesetor);
+	
+	printf("Informe a quantidade de produtos do setor %i: ", index);
+	scanf("%i", &qtd);
+	paux->qtde = qtd;
+	
+	paux->prod = armazena_produto(qtd, index);
+    printf("\n");
+}
+
+TProduto *armazena_produto(int qtd, int index){
+	TProduto *aux = (TProduto*) calloc(qtd, sizeof(TProduto));
+	if (!aux) handle_error();
+
+    char buf[20];
+
+	for (int i = 0; i < qtd; i++) {
+		printf("\nNome do produto %i do setor %i: ", i + 1, index);
+		scanf("%s", buf);
+        strcpy((aux + i)->nomeproduto, buf);
+
+		printf("Valor do produto %i do setor %i: ", i + 1, index);
+		scanf("%f", &(aux + i)->valor);
+	}
+    printf("\n");
+	return aux;
+}
+
+TSetor *desaloca_setor(TSetor *p, int tam) {
+	for (int i = 0; i < tam; i++) free((p + i)->prod);
+	free(p);
+
+	return NULL;
+}
+
+void handle_error() {
+    printf("Ma alocacao\n");
+    exit(EXIT_FAILURE);
+}
---
+++ Data_Structures/Tests/P1_lab.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct funcionario{
+      char nome[20];
+      int  idade; 
+      int  sexo; 
+}TFuncionario;
+
+TFuncionario *load_file(char *file);
+void free_(TFuncionario *v);
+void distribute(TFuncionario *v);
+void handle_error(int err);
+
+int line_count = 1;
+
+int main()
+{
+    //definicoes de variaveis
+    int op=1;
+    char file_name[255];
+    TFuncionario *v = NULL;
+
+    while(op!=3)
+    {
+	  printf("\n1-Carrega arquivo\n2-Distribui funcionario\n3-Sair\nInforme opcao:");
+      scanf("%d",&op);
+      switch(op)
+      {
+      	case 1: {
+          printf("Nome do arquivo: \n"); scanf("%s", file_name);
+          v = load_file(file_name);
+        } continue;
+		    case 2: {
+          distribute(v);
+        } continue;
+  	  }
+  	  //....
+  	}
+    free_(v);
+    return 0;
+}
+
+TFuncionario *load_file(char *file) {
+  FILE *f = fopen(file, "r");
+  if (!f) handle_error(1);
+
+  char c;
+  while (!feof(f)) {
+    fread(&c, 1, 1, f);
+    if (c == '\n') line_count++;
+  }
+  
+  fseek(f, 0, SEEK_SET);
+  
+  TFuncionario *tmp = (TFuncionario*) calloc(line_count, sizeof(TFuncionario));
+  if (!tmp) handle_error(2);
+
+  char buf[20];
+  for (int i = 0; i < line_count; i++) {
+    fscanf(f, "%s", buf);
+    strcpy(tmp[i].nome, buf);
+
+    fscanf(f, "%i", &(tmp[i].idade));
+    fscanf(f, "%i", &(tmp[i].sexo));
+  }
+  fclose(f);
+  return tmp;
+}
+
+void distribute(TFuncionario *v) {
+  float team_size = (float) line_count / 3.0;
+  if ((int) (team_size * 10) % 10 == 0) {
+    int t1 = team_size;
+    int t2 = team_size;
+    int t3 = team_size;
+  }
+  int t1 = team_size + 1;
+  int t2 = team_size;
+  int t3 = team_size;
+  
+  FILE *ft1 = fopen("equipe1.txt", "w");
+  FILE *ft2 = fopen("equipe2.txt", "w");
+  FILE *ft3 = fopen("equipe3.txt", "w");
+
+  // nao deu tempo de implementar as regras de divisao :(
+
+  for (int i = 0; i < t1 - 1; i++) {
+    fprintf(ft1, "%s %i %i\n", v[i].nome, v[i].idade, v[i].sexo);
+  }
+  for (int i = t1; i < t1 + t2 - 1; i++) {
+    fprintf(ft2, "%s %i %i\n", v[i].nome, v[i].idade, v[i].sexo);
+  }
+  for (int i = t1 + t2; i < t1 + t2 + t3 - 1; i++) {
+    fprintf(ft3, "%s %i %i\n", v[i].nome, v[i].idade, v[i].sexo);
+  }
+
+
+  fclose(ft1);
+  fclose(ft2);
+  fclose(ft3);
+}
+
+void free_(TFuncionario *v) {
+  free(v);
+}
+
+void handle_error(int err) {
+  if (err == 1) {
+    printf("Nao foi possivel abrir esse abrir esse arquivo.\n");
+    exit(0);
+  }
+  printf("Nao foi possivel alocar os funcionarios\n");
+  exit(0);
+}
