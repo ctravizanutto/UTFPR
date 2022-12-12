620790f89e18a3d202a0288c6e19770afe6ca1b8
travizanutto <ctravizanutto@gmail.com>
2022-12-11 23:43:40 -0300

    data structures 2 assignment

---
+++ Data_Structures/Data_Structures2/main.c
@@ -0,0 +1,18 @@
+/*
+    Cainan Travizanutto
+    Caio Furlan
+    Gabriel Costa Maestre
+    Augusto Padilha
+    
+    Estrutura de Dados 2 - UTFPR
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "libs/mtwister.h"
+
+static MTRand rand_num = seedRand(123456);
+
+int main(void) {
+    
+    
+}
